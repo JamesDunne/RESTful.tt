41aa5ed233444785a967446aa308d86b6daf1d88
James Dunne <james.jdunne@gmail.com>
2012-11-12 14:41:17 -0600

    Sample code using CalendarList data contract object instead of JObject. Pretty formatting now.

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using WellDunne.REST;
 using WellDunne.REST.Google;
+using WellDunne.REST.Google.V3;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Google.Apis.Calendar.v3.Data;
 
 namespace TestClient
 {
@@ -19,18 +21,20 @@ namespace TestClient
 
             // Create the client against googleapis.com:
             // NOTE: This client instance is reusable across threads.
-            var client = new WellDunne.REST.Google.V3.CalendarServiceAsyncClient(new Uri("https://www.googleapis.com"), oauth);
+            var client = new CalendarServiceAsyncClient(new Uri("https://www.googleapis.com"), oauth);
 
             // Create the request to get the user's calendar list:
             // NOTE: Each request object is NOT reusable.
             var req = client.GetMyCalendarList(null, null, null, /*requestorID:*/ "username@example.com");
 
             // Fetch the request synchronously:
-            var rsp = req.Fetch<JObject>();
+            var rsp = req.Fetch<CalendarList>();
 
             // Write the response JSON object to Console.Out:
             using (var conWriter = new JsonTextWriter(Console.Out))
-                rsp.Response.WriteTo(conWriter);
+                JsonSerializer.Create(new JsonSerializerSettings { Formatting = Formatting.Indented })
+                    .Serialize(conWriter, rsp.Response);
+
             Console.WriteLine();
         }
     }
